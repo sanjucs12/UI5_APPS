1d0ab1868de93c54064626195dbd63d66c50a805
Sanju C S <sanju@airditsoftware.com>
2024-01-22 12:56:05 +0000

    graph - first node - last node - parallel middle nodes done

--- metadata copy/webapp/controller/View2.controller.js
+++ metadata copy/webapp/controller/View2.controller.js
@@ -506,24 +506,28 @@ sap.ui.define([
 
             /////////////>>>>>>>>>>>>>>>>>>>NETWORK GRAPH<<<<<<<<<<<<<<<<<<<<<<////////////////////////////////
             _createNetworkGraph: function (data) {
-
-                //var oNetworkModel = new sap.ui.model.json.JSONModel();               
+             
                 const nodes = data.map(step => ({ ...step }));
+
+                const firstNode = nodes[0];
+                const lastNode = nodes[nodes.length-1];
+                
                 const lines = [];
-                for (let i = 0; i < data.length - 1; i++) {
-                    lines.push({
-                        from: data[i].StepId,
-                        to: data[i + 1].StepId
-                    });
+                for (let i = 1; i < nodes.length-1; i++) {
+                    lines.push({ from: firstNode.StepId, to: nodes[i].StepId });
+                    lines.push({ from: nodes[i].StepId, to: lastNode.StepId });
                 }
+                // for (let i = 1; i < nodes.length-1; i++) {
+                //     lines.push({ from: nodes[i].StepId, to: lastNode.StepId });
+                // }
 
                 // Create final result object
-                const result = {
+                const oGraphData = {
                     nodes,
                     lines,
                 };
-                //console.log(result)
-                var oNetworkModel = new sap.ui.model.json.JSONModel(result);
+                console.log(oGraphData)
+                var oNetworkModel = new sap.ui.model.json.JSONModel(oGraphData);
                 //console.log(oNetworkModel.getData())
                 this.getView().byId("networkGraph").setModel(oNetworkModel)
             }
--- network_graph/webapp/controller/View1.controller.js
+++ network_graph/webapp/controller/View1.controller.js
@@ -26,15 +26,20 @@ sap.ui.define([
                 })
             },
             _createNetworkGraph: function (data) {
-
-                //var oNetworkModel = new sap.ui.model.json.JSONModel();               
+               
                 const nodes = data.map(step => ({ ...step }));
+
+                const firstNode = nodes[0];
+                const lastNode = nodes[nodes.length-1];
+                
+                // Create lines array connecting all nodes
                 const lines = [];
-                for (let i = 0; i < data.length - 1; i++) {
-                    lines.push({
-                        from: data[i].StepId,
-                        to: data[i + 1].StepId
-                    });
+                for (let i = 1; i < nodes.length-1; i++) {
+                    lines.push({ from: firstNode.StepId, to: nodes[i].StepId });
+                    //lines.push({ from: nodes[i].StepId, to: lastNodeId });
+                }
+                for (let i = 1; i < nodes.length-1; i++) {
+                    lines.push({ from: nodes[i].StepId, to: lastNode.StepId });
                 }
 
                 // Create final result object
